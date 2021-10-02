e4313742b21c1ffcc4484a3ffccbd4bb89b41087
Allen Ladd <ghost2130@gmail.com>
2021-10-02 10:23:48 -0400

    started the typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String!
+    link: String!
+    title: String!
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int!
+    savedBooks: [Book]
+  }
+
+  input savedBook {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String!
+    link: String!
+    title: String!
+  }
+
+  type Query {
+    me: User
+  }
+`;
